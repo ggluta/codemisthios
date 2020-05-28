9957b9043713971b690920fb0b4e7cd124e2f2fc
George Gabriel Luta <gg@GGs-MacBook-Pro.local>
2020-05-29 00:41:36 +0200

    added lottie animation

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,42 @@
+/* eslint-disable import/extensions */
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <lottie-player
+      class="lottie-animtation"
+      autoplay
+      loop
+      mode="normal"
+      src='https://assets6.lottiefiles.com/packages/lf20_MAiPvC.json'>
+</lottie-player>
   </div>
 </template>
+
+<script>
+import '@lottiefiles/lottie-player';
+
+export default {
+  components: {
+  },
+};
+</script>
 
 <style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
+  background-color: #FFFFFF;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  padding: 0;
+  margin: 0;
+  width: 100%;
+  height: 100vh;
+}
+
+.lottie-aniamtion {
+  width: 100%;
+  height: 100vh;
 }
 
 #nav {
